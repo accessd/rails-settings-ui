d70385ae25b176dd989530a61c2e734802a4d0cb
Accessd <accessd0@gmail.com>
2014-07-30 13:14:01 +0400

    fix bootstrap 2 dependency

--- lib/rails-settings-ui/engine.rb
+++ lib/rails-settings-ui/engine.rb
@@ -7,6 +7,10 @@ module ::RailsSettingsUi
   class Engine < Rails::Engine
     isolate_namespace RailsSettingsUi
 
+    initializer "assets_precompile" do |app|
+      app.config.assets.precompile += %w( glyphicons-halflings.png glyphicons-halflings-white.png)
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
