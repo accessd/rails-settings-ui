1765c9fc2434bfe63e0e3cb5bce9e350e157eff8
MichaelSp <MichaelSp@users.noreply.github.com>
2014-12-03 11:52:14 +0100

    Add support for ActionController::Parameters

--- lib/rails-settings-ui/value_types/array.rb
+++ lib/rails-settings-ui/value_types/array.rb
@@ -7,6 +7,8 @@ module RailsSettingsUi
         # Array presented in checkboxes
         if ["Hash", "ActiveSupport::HashWithIndifferentAccess"].include?(value.class.name)
           value.keys.map!(&:to_sym)
+        elsif value.class.name == 'ActionController::Parameters'
+          value.reject{|_,v| v!='on' }.keys.map!(&:to_sym)
         # or in select tag
         else
           value.to_sym
